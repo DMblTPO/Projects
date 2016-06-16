c65b1f935c7978350becdc1d449bfbc262b32dc1
Dmytro Boboshko <DBoboshko@cloudroute.com>
2016-06-16 15:38:57 +0300

    adding controller methods and views

--- Qalsql/Qalsql/Controllers/SqlCheckerController.cs
+++ Qalsql/Qalsql/Controllers/SqlCheckerController.cs
@@ -1,4 +1,6 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
 using Qalsql.Models;
 
 namespace Qalsql.Controllers
@@ -14,8 +16,52 @@ namespace Qalsql.Controllers
         [HttpPost]
         public ActionResult SendSql(string sql)
         {
-            var model = SqlExecutor.SendQuery(sql);
-            return View(model);
+            SqlResult sqlResult;
+            if (ModelState.IsValid)
+            {
+                sqlResult = SqlExecutor.SendQuery(sql);
+            }
+            else
+            {
+                sqlResult = CreatorSqlResult.Fail(sql,
+                    ModelState.Values
+                        .SelectMany(m => m.Errors, (parent, child) => child.ErrorMessage)
+                        .Aggregate((s, n) => s + ";" + n));
+            }
+            return View(sqlResult);
+        }
+
+        public ActionResult CheckSql()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CheckSql(int lessonId, int taskId, string sql)
+        {
+            string mainSql = SqlTasks.Get(lessonId, taskId);
+
+            string combainedSql = String.Format("{0} except {1} union all {1} except {0}", sql, mainSql);
+
+            SqlResult checkResult = SqlExecutor.SendQuery(combainedSql);
+            SqlResult testingSql = SqlExecutor.SendQuery(sql);
+
+            if (checkResult.Status.IsOk)
+            {
+                return View("CheckSqlOk");
+            }
+
+            SqlResult etalonResult = SqlExecutor.SendQuery(mainSql);
+
+            return View("CheckSqlFail");
+        }
+    }
+
+    public static class SqlTasks
+    {
+        public static string Get(int lessonId, int taskId)
+        {
+            return "select top(1) * from Students";
         }
     }
 }
