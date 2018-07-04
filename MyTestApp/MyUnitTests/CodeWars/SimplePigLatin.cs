1fe2fed56d7cecccadc0e8a2d6db0b95de1dab1f
Dmytro Boboshko <xepomaht@gmail.com>
2018-07-04 22:20:12 +0300

    Add folder and restruct proj

---
+++ MyTestApp/MyUnitTests/CodeWars/SimplePigLatin.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[TestClass]
+public class SimplePigLatin
+{
+    [TestMethod]
+    public void KataTests()
+    {
+        Assert.AreEqual("igPay atinlay siay oolcay", Kata.PigIt("Pig latin is cool"));
+        Assert.AreEqual("hisTay siay ymay tringsay", Kata.PigIt("This is my string"));
+    }
+}
+
+public class Kata
+{
+    static StringBuilder _sb = new StringBuilder();
+
+    public static string PigIt(string str)
+    {
+        _sb.Clear();
+        foreach (var s in str.Split(' '))
+        {
+            if (_sb.Length > 0) _sb.Append(" ");
+            _sb.Append(s.Substring(1));
+            _sb.Append(s[0]);
+            _sb.Append("ay");
+        }
+        return _sb.ToString();
+    }
+}
