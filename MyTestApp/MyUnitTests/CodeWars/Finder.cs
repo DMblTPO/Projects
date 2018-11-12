45da42316124342ed03f3d4ce1b7867613d52c04
DmytroBoboshko <dmytro.boboshko@gmail.com>
2018-11-12 17:43:02 +0200

    New tests and katas

---
+++ MyTestApp/MyUnitTests/CodeWars/Finder.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Remoting.Channels;
+using NUnit.Framework;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+namespace MyUnitTests.CodeWars
+{
+    public class SolutionTest
+    {
+
+        [Test]
+        public void TestBasic()
+        {
+
+            string a = ".W.\n" +
+                       ".W.\n" +
+                       "...",
+
+                b = ".W.\n" +
+                    ".W.\n" +
+                    "W..",
+
+                c = "......\n" +
+                    "......\n" +
+                    "......\n" +
+                    "......\n" +
+                    "......\n" +
+                    "......",
+
+                d = "......\n" +
+                    "......\n" +
+                    "......\n" +
+                    "......\n" +
+                    ".....W\n" +
+                    "....W.";
+
+            Assert.AreEqual(4, Finder.PathFinder(a));
+            Assert.AreEqual(-1, Finder.PathFinder(b));
+            Assert.AreEqual(10, Finder.PathFinder(c));
+            Assert.AreEqual(-1, Finder.PathFinder(d));
+        }
+    }
+
+    public class Finder
+    {
+        public static int PathFinder(string mazeTxt)
+        {
+            var maze = new Maze(mazeTxt);
+
+            while (maze.Current == maze.Finish)
+            {
+                var pos = maze.GoEast() ?? maze.GoSouth() ?? maze.GoWest() ?? maze.GoWest();
+
+            }
+
+            return 4;
+        }
+
+    }
+
+    public class Maze
+    {
+        private int _edge;
+        private string[] _mazeMap;
+        private readonly Position _start;
+        private readonly Position _finish;
+        private Position _cur;
+
+        public Maze(string maze)
+        {
+            _mazeMap = maze.Split(new[] {'\n'});
+            _edge = _mazeMap.Length - 1;
+            _start = new Position(0, 0);
+            _cur = new Position(0, 0);
+            _finish = new Position(_edge, _edge);
+        }
+
+        public Position Start => _start;
+        public Position Finish => _finish;
+        public Position Current => _cur;
+
+        public Position GoNorth()
+            => Validate(_cur.X-1, _cur.Y) ? _cur = new Position(_cur.X-1, _cur.Y) : null;
+
+        public Position GoSouth()
+            => Validate(_cur.X+1, _cur.Y) ? _cur = new Position(_cur.X+1, _cur.Y) : null;
+
+        public Position GoWest()
+            => Validate(_cur.X, _cur.Y-1) ? _cur = new Position(_cur.X, _cur.Y-1) : null;
+
+        public Position GoEast()
+            => Validate(_cur.X, _cur.Y+1) ? _cur = new Position(_cur.X, _cur.Y+1) : null;
+
+        private bool Validate(int x, int y) => x >= 0 && y >= 0 && x <= _edge && y <= _edge;
+
+        public class Position
+        {
+            public Position(int x, int y)
+            {
+                X = x;
+                Y = y;
+            }
+            public int X { get; }
+            public int Y { get; }
+            public (int x, int y) XY => (X, Y);
+
+            public override bool Equals(object obj)
+            {
+                if (obj is Position p)
+                {
+                    return p == this;
+                }
+                return base.Equals(obj);
+            }
+
+            public static bool operator ==(Position rp, Position lp) => rp != null && lp != null && rp.Y == lp.Y && rp.X == lp.X;
+            public static bool operator !=(Position rp, Position lp) => rp != null && lp != null && (rp.Y != lp.Y || rp.X != lp.X);
+        }
+
+        public enum Direction
+        {
+            North, East, South, West
+        }
+    }
+
+}
---
+++ MyTestApp/MyUnitTests/CodeWars/HumanTimeFormat.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization.Formatters;
+using System.Text;
+
+namespace MyUnitTests.CodeWars
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class TestTimes
+    {
+        [Test]
+        public void test1()
+        {
+            Assert.AreEqual("now", HumanTimeFormat.formatDuration(0));
+        }
+
+        [Test]
+        public void test2()
+        {
+            Assert.AreEqual("1 second", HumanTimeFormat.formatDuration(1));
+        }
+
+        [Test]
+        public void test3()
+        {
+            Assert.AreEqual("1 minute and 2 seconds", HumanTimeFormat.formatDuration(62));
+        }
+
+        [Test]
+        public void test4()
+        {
+            Assert.AreEqual("2 minutes", HumanTimeFormat.formatDuration(120));
+        }
+
+        [Test]
+        public void test5()
+        {
+            Assert.AreEqual("1 hour, 1 minute and 2 seconds", HumanTimeFormat.formatDuration(3662));
+        }
+
+        [Test]
+        public void test6()
+        {
+            Assert.AreEqual("3 years, 7 days, 15 hours, 23 minutes and 12 seconds", HumanTimeFormat.formatDuration(95268192));
+        }
+    }
+
+    public class HumanTimeFormat
+    {
+        public static string formatDuration(int seconds)
+        {
+            if (seconds <= 0)
+            {
+                return "now";
+            }
+
+            const long secs = 60;
+            const long mins = secs * 60;
+            const long hours = mins * 24;
+            const long days = hours * 365;
+
+            var daysLeft = seconds % days;
+            var hoursLeft = daysLeft % hours;
+            var minsLeft = hoursLeft % mins;
+
+            var yy = seconds / days;
+            var dd = daysLeft / hours;
+            var hh = hoursLeft / mins;
+            var mm = minsLeft / secs;
+            var ss = minsLeft % secs;
+
+            var list = new List<string>();
+
+            string AddPlural(long x) => x > 1 ? "s" : string.Empty;
+
+            void AddTimePart(long x, string name)
+            {
+                if(x > 0) list.Add($"{x} {name}{AddPlural(x)}");
+            }
+
+            AddTimePart(yy, "year");
+            AddTimePart(dd, "day");
+            AddTimePart(hh, "hour");
+            AddTimePart(mm, "minute");
+            AddTimePart(ss, "second");
+
+            var count = list.Count;
+
+            return count == 1 ? list.First() : $"{string.Join(", ", list.Take(count - 1))} and {list.Last()}";
+        }
+    }
+}
